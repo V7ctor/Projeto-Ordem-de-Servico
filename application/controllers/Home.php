e235eb0d3a8152243f67cdeb45a534a48105da36
V7ctor <joaovictor89437@gmail.com>
2022-03-21 13:02:34 -0300

    Fatiando o Template

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -8,7 +8,9 @@ class Home extends CI_Controller {
     }
 
     public function index() {
+        $this->load->view("layout/header");
         $this->load->view("home/index");
+        $this->load->view("layout/footer");
     }
 }
 ?>
